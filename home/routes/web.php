4de8fd65342e6f2bbb1eb77e9f7b50867062b8d0
husnazack <saidatulzhusna@gmail.com>
2024-04-15 16:06:50 +0800

    q

--- home/routes/web.php
+++ home/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\postController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
